cd42188efd92e5fd4497b80e4b97225dcad4f870
JiaoJinxin <jiaojinxin220419@credithc.com>
2024-02-29 13:34:40 +0800

    feat: jln-boot/jln-cloud整合

--- jln-system/jln-system-log/src/main/java/top/jiaojinxin/system/configuration/SystemLogAutoConfiguration.java
+++ jln-system/jln-system-log/src/main/java/top/jiaojinxin/system/configuration/SystemLogAutoConfiguration.java
@@ -1,16 +1,15 @@
 package top.jiaojinxin.system.configuration;
 
-import com.alibaba.fastjson2.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import top.jiaojinxin.log.audit.aspect.LogHandler;
+import top.jiaojinxin.log.audit.configuration.AuditLogAutoConfiguration;
 import top.jiaojinxin.system.log.handler.AuditLogHandler;
 import top.jiaojinxin.system.log.handler.LogDetailsHandler;
 import top.jiaojinxin.system.properties.LogProperties;
-import top.jiaojinxin.log.audit.aspect.LogHandler;
-import top.jiaojinxin.log.audit.configuration.AuditLogAutoConfiguration;
 
 /**
  * 审计日志自动装配
@@ -24,7 +23,7 @@ public class SystemLogAutoConfiguration {
 
     @Bean
     public LogDetailsHandler defaultLogDetailsHandler() {
-        return auditLogDetails -> log.info("{}", JSON.toJSONString(auditLogDetails));
+        return auditLogDetails -> log.info("{}", auditLogDetails.toString());
     }
 
     @Bean
